4af748eb098206114dd34d1bf656ff37c18ba533
Andrea Sacerdoti <andrea.sacerdoti@icloud.com>
2024-01-20 02:25:51 +0100

    Update swift-tools-version

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "mockable-stores",
-  platforms: [.iOS(.v11), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
+  platforms: [.iOS(.v12), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v9)],
   products: [
     .library(
       name: "MockableStores",
